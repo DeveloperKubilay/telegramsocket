efcd1d8ae97212f468a6edcd58d5c2efb8c7eb98
Kubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2024-11-22 03:24:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const telegrambot = require('./modules/serve')('5294567893:BBFYdRlp8EQ7ur8FwcyHYXZwD4jZi3b6lpo9'); // write ur bot token
 
 telegrambot.on('connection', (client) => {
-    client.write("yo whatspp")
     
     console.log(`Connection sucsess`);
+    client.write("yo whatspp")
+        
     client.on('data', (data) => {
         console.log("my bro sayed "+data);
     });
-    client.on('end', (data) => {
+
+    client.on('close', (data) => {
         console.log(`:/`);
     });
-
+   // setTimeout(()=> client.end(), 2000)
 });
